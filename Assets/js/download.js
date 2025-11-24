2904edcb64ae99d925b2772e8c7d89a083485598
Naresh Narnapati <naresh.narnapati@reymould.com>
2025-11-24 17:09:19 +0530

    Download functionalit added

---
+++ Assets/js/download.js
@@ -0,0 +1,10 @@
+$('#downloadForm').on('click', function (e) {
+    e.preventDefault();
+    const url = '/accuanalytical/Assets/images/test-request-form.pdf';
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'test-request-form.pdf';
+    document.body.appendChild(a);
+    a.click();
+    a.remove();
+});
